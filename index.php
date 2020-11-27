198612d84778bd6cf00d2f90772a0862227cff87
quangmai2k <1851061486@e.tlu.edu.vn>
2020-11-27 14:12:57 +0700

    Hello

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Omnifood</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="row">
+                <img src="data/logo-white.png" alt="logo Omnifood" class="logo">
+                <ul class="main-nav">
+                    <li><a href="#">Food delivery</a></li>
+                    <li><a href="#">How it works</a></li>
+                    <li><a href="#">Our cities</a></li>
+                    <li><a href="#">Sign up</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="hero-text-box">
+            <h1>Goodbye junk food. <br>Hello super healthy meals</h1>
+            <a class="btn btn-full" href="#">I'm hungry</a>
+            <a class="btn btn-ghost" href="#">Show me more</a>
+        </div>
+    </header>
+</body>
+</html>
